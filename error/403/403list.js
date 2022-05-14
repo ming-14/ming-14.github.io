9a21ffa52ceb9ee432d048fc9e666f7ffc2945a8
HelloWorld <104149332+ming-14@users.noreply.github.com>
2022-05-14 10:39:45 +0800

    Add files via upload

---
+++ error/403/403list.js
@@ -0,0 +1,9 @@
+var MobileDevices = [
+        "/error/403/403-PixelLock/index.html", 
+        "/error/403/403-ErrorCode/index.html"
+    ];
+    
+var Computer = [
+        "/error/403/403-PixelLock/index.html", 
+        "/error/403/403-ErrorCode/index.html"
+    ];
