96718aa51c8d1a37336852998c3fe17919729310
HelloWorld <104149332+ming-14@users.noreply.github.com>
2022-05-14 10:49:58 +0800

    Add files via upload

---
+++ error/404/404list.js
@@ -0,0 +1,15 @@
+var MobileDevices = [
+        "/error/404/TencentPublicWelfare404.html", 
+        "/error/404/Chrome404Game/index.html",
+        "/error/404/404-business-office-interface/index.html",
+        "/error/404/404-delicious-food/index.html", 
+        "/error/404/404-simple-mistake/index.html"
+    ];
+    
+var Computer = [
+        "/error/404/TencentPublicWelfare404/TencentPublicWelfare404.html", 
+        "/error/404/Chrome404Game/index.html",
+        "/error/404/404-business-office-interface/index.html", 
+        "/error/404/404-delicious-food/index.html",
+        "/error/404/404-simple-mistake/index.html"
+    ];
